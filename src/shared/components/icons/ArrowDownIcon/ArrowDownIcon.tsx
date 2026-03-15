00abc9e5e8cc2fd6553b6b9687b327a8ce805407
Elena Diatlenko <lenadea199900@gmail.com>
2026-03-15 15:07:51 +0300

    feat: add animation for multidropdawn

--- src/shared/components/icons/ArrowDownIcon/ArrowDownIcon.tsx
+++ src/shared/components/icons/ArrowDownIcon/ArrowDownIcon.tsx
@@ -4,8 +4,8 @@ import Icon, { type IconProps } from '../Icon';
 
 const ArrowDownIcon: React.FC<IconProps> = ({
   color,
-  width = 35,
-  height = 35,
+  width = 24,
+  height = 24,
   className,
   ...props
 }) => {
@@ -15,9 +15,9 @@ const ArrowDownIcon: React.FC<IconProps> = ({
         strokeLinejoin="round"
         strokeLinecap="round"
         strokeMiterlimit={10}
-        strokeWidth={2}
+        strokeWidth={2.5}
         fill="none"
-        d="M4.08004 8.91003L10.6 15.43C11.37 16.2 12.63 16.2 13.4 15.43L19.92 8.91003"
+        d="M4.07998 8.91L10.6 15.43C11.37 16.2 12.63 16.2 13.4 15.43L19.92 8.91"
       />
     </Icon>
   );
