6cd3b652cb1d2c2c55e51990147e4a3a272e6d47
Elena Diatlenko <lenadea199900@gmail.com>
2026-03-19 02:41:14 +0300

    feat: add login modal for like

---
+++ src/shared/components/ModalLogin/ModalLogin.tsx
@@ -0,0 +1,43 @@
+import Link from 'next/link';
+import { useRouter } from 'next/navigation';
+
+import { ROUTES } from '@/shared/config/routes';
+import { useRootStore } from '@/shared/stores/context/RootContext';
+import { AnalyticsEvent } from '@/shared/stores/models/analytics';
+
+import Button from '../Button';
+import Text from '../Text';
+
+import styles from './ModalLogin.module.scss';
+
+export type ModalLoginProps = {
+  text: string;
+  description?: string;
+};
+
+const ModalLogin: React.FC<ModalLoginProps> = ({ text, description }) => {
+  const router = useRouter();
+  const { analyticsStore } = useRootStore();
+
+  const handleClickLogin = () => {
+    router.push(ROUTES.LOGIN);
+  };
+
+  const handleClickSignUp = () => {
+    analyticsStore.sendEvent(AnalyticsEvent.clickSignUp, { fromLocation: 'modal' });
+  };
+
+  return (
+    <div className={styles.modalLogin}>
+      <Text view="p-l" weight="bold">
+        {text}
+      </Text>
+      {description && <Text view="p-m">{description}</Text>}
+      <Button onClick={handleClickLogin}>Войти</Button>
+      <Link href={ROUTES.SING_UP} onClick={handleClickSignUp}>
+        Зарегистрироваться
+      </Link>
+    </div>
+  );
+};
+export default ModalLogin;
